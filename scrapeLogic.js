c4802ed7a639aa69676579f8a82a170010fc63e8
ys-grouse <malsawma7777@gmail.com>
2023-05-27 20:40:43 +0530

    update function

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -17,27 +17,43 @@ const scrapeLogic = async (res) => {
   try {
     const page = await browser.newPage();
 
-    await page.goto("https://developer.chrome.com/");
+    // await page.goto("https://developer.chrome.com/");
+    await page.setContent('test')
 
+    const pdf = await page.pdf({
+      path: "output.pdf",
+      format: "A4",
+      printBackground: true,
+      encoding: "base64"
+    });
+
+    res.set({
+      // 'Content-Disposition': 'attachment; filename="output.pdf"',
+      "Content-Type": "application/pdf",
+      "Content-Length": pdf.length
+    });
+
+
+    res.send(pdf);
     // Set screen size
-    await page.setViewport({ width: 1080, height: 1024 });
+    // await page.setViewport({ width: 1080, height: 1024 });
 
-    // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder");
+    // // Type into search box
+    // await page.type(".search-box__input", "automate beyond recorder");
 
-    // Wait and click on first result
-    const searchResultSelector = ".search-box__link";
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
+    // // Wait and click on first result
+    // const searchResultSelector = ".search-box__link";
+    // await page.waitForSelector(searchResultSelector);
+    // await page.click(searchResultSelector);
 
-    // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector(
-      "text/Customize and automate"
-    );
-    const fullTitle = await textSelector.evaluate((el) => el.textContent);
+    // // Locate the full title with a unique string
+    // const textSelector = await page.waitForSelector(
+    //   "text/Customize and automate"
+    // );
+    // const fullTitle = await textSelector.evaluate((el) => el.textContent);
 
-    // Print the full title
-    const logStatement = `The title of this blog post is ${fullTitle}`;
+    // // Print the full title
+    // const logStatement = `The title of this blog post is ${fullTitle}`;
     console.log(logStatement);
     res.send(logStatement);
   } catch (e) {
